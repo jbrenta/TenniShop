8d1e76644d9bb5f3f397a627df9aa8e3b34cb9a3
jbrenta <jbrenta@studenti.apuliadigitalmaker.it>
2025-06-03 13:54:52 +0200

    Update image paths

--- src/data/images.js
+++ src/data/images.js
@@ -1,33 +1,33 @@
 // Banner Images
 export const bannerImages = {
-  boom: '/assets/head-boom-banner.jpg',
-  pure: '/assets/pure-drive-gen11.jpg',
-  ezone: '/assets/ezone-2025.jpg',
-  sinner: '/assets/sinner-ao25.jpg',
+  boom: '/TenniShop/assets/head-boom-banner.jpg',
+  pure: '/TenniShop/assets/pure-drive-gen11.jpg',
+  ezone: '/TenniShop/assets/ezone-2025.jpg',
+  sinner: '/TenniShop/assets/sinner-ao25.jpg',
 };
 
 // Category Images
 export const categoryImages = {
-  racchetta: '/assets/racchetta.webp',
-  maglietta: '/assets/maglia.jpg',
-  scarpa: '/assets/scarpa.jpg',
-  borsone: '/assets/borsone.jpg',
+  racchetta: '/TenniShop/assets/racchetta.webp',
+  maglietta: '/TenniShop/assets/maglia.jpg',
+  scarpa: '/TenniShop/assets/scarpa.jpg',
+  borsone: '/TenniShop/assets/borsone.jpg',
 };
 
 // Brand Logos
 export const brandLogos = {
-  adidas: '/assets/loghi/adidas.jpg',
-  asics: '/assets/loghi/asics.jpg',
-  babolat: '/assets/loghi/babolat.png',
-  head: '/assets/loghi/Head-Logo.jpg',
-  nike: '/assets/loghi/nike.jpg',
-  wilson: '/assets/loghi/wilson.webp',
+  adidas: '/TenniShop/assets/loghi/adidas.jpg',
+  asics: '/TenniShop/assets/loghi/asics.jpg',
+  babolat: '/TenniShop/assets/loghi/babolat.png',
+  head: '/TenniShop/assets/loghi/Head-Logo.jpg',
+  nike: '/TenniShop/assets/loghi/nike.jpg',
+  wilson: '/TenniShop/assets/loghi/wilson.webp',
 };
 
 // Categories Banner Images
 export const categoryBanners = {
-  head: '/assets/categories/CategoriesHead.webp',
-  babolat: '/assets/categories/CategoriesBabolat.jpg',
-  wilson: '/assets/categories/CategoriesWilson.webp',
-  adidas: '/assets/categories/CategoriesAdidas.jpeg',
+  head: '/TenniShop/assets/categories/CategoriesHead.webp',
+  babolat: '/TenniShop/assets/categories/CategoriesBabolat.jpg',
+  wilson: '/TenniShop/assets/categories/CategoriesWilson.webp',
+  adidas: '/TenniShop/assets/categories/CategoriesAdidas.jpeg',
 }; 
